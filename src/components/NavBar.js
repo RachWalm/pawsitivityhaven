b398a1dc421b479d24f20085f2315d9b6415ec17
Rachel Walmsley <rachwalm75@gmail.com>
2024-04-29 10:03:41 +0000

    Style. Navbar and background styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar, Container, Nav, } from "react-bootstrap"
 import logo from "../assets/logo.png"
+import styles from "../styles/NavBar.module.css"
+
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <Navbar.Brand>
                 <img src={logo} height="50" alt="logo"/>
@@ -12,7 +14,7 @@ const NavBar = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav className="ml-auto text-right">
                 <Nav.Link>
                     <i className="fa-solid fa-house-chimney"></i>
                     Home
