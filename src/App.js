1eead15f3d5f5ffab0b42179c7461915af03b640
Rachel Walmsley <rachwalm75@gmail.com>
2024-05-02 09:25:06 +0000

    Feat Add boilerplate to createdogprofile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Switch} from "react-router-dom";
 import "./api/axiosDefault";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import DogProfileCreateForm from "./pages/dogProfile/DogProfileCreateForm";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/dog-profile/create" render={() => <DogProfileCreateForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
