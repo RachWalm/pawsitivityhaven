8b02d307236a14b4c600ed51c5b93c3a003fce8e
Rachel Walmsley <rachwalm75@gmail.com>
2024-05-02 09:04:44 +0000

    Feat Add close burger menu function using hooks in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,14 @@ import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import axios from "axios"
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -45,7 +48,11 @@ const NavBar = () => {
         </NavLink>
         </>
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+        expanded={expanded}
+        className={styles.NavBar}
+        expand="md" fixed="top"
+    >
         <Container>
             <NavLink to="/">
                 <Navbar.Brand>
@@ -54,7 +61,11 @@ const NavBar = () => {
             </NavLink>
             <h1>Pawsitive Haven</h1>
             {currentUser && addPostIcon}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle
+                ref={ref}
+                onClick={() => setExpanded(!expanded)}
+                aria-controls="basic-navbar-nav"
+            />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-right">
                 <NavLink exact to="/">
