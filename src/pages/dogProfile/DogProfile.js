92534f0fafa916f1fea274d01bbf182cdaa83f60
Rachel Walmsley <rachwalm75@gmail.com>
2024-05-06 15:57:05 +0000

    Feat add functions to change numbers to text in dog profile

--- src/pages/dogProfile/DogProfile.js
+++ src/pages/dogProfile/DogProfile.js
@@ -50,6 +50,8 @@ function DogProfile() {
         setDogData({ dog_name, received_date, rehomed_date, returned_date, dog_age, dog_breed, dog_gender,
           dog_size, dog_image, at_rescue, status, general, home_cats, home_dogs, home_animals,
           home_children,  });
+          console.log('anmi')
+          console.log(data.home_dogs)
       } catch (err) {
         console.log(err);
       }
@@ -57,6 +59,47 @@ function DogProfile() {
 
     handleMount();
   }, [history, id]);
+
+  const getDogGender = (dog_gender) => {
+    switch (dog_gender) {
+      case 0:
+        return 'TBC'; // To Be Confirmed
+      case 1:
+        return 'Male';
+      case 2:
+        return 'Female';
+      default:
+        return 'Unknown'; // Default case for unexpected input
+    }
+  };
+
+  const getDogSize = (dog_size) => {
+    switch (dog_size) {
+      case 0:
+        return 'TBC'; // To Be Confirmed
+      case 1:
+        return 'Small';
+      case 2:
+        return 'Large';
+      default:
+        return 'Unknown'; // Default case for unexpected input
+    }
+  };
+
+  const getDogStatus = (status) => {
+    switch (status) {
+      case 0:
+        return 'To arrive'; 
+      case 1:
+        return 'Not available';
+      case 2:
+        return 'Available';
+      case 3:
+      return 'Rehomed';
+      default:
+        return 'unknown'; // Default case for unexpected input
+    }
+  };
 
   const mainProfile = (
     <>
@@ -68,11 +111,12 @@ function DogProfile() {
           <h3 className="m-2"> returned_date - {returned_date} </h3>
           <h3 className="m-2"> dog_age - {dog_age} </h3>
           <h3 className="m-2"> dog_breed - {dog_breed} </h3>
-          <h3 className="m-2"> dog_gender - {dog_gender} </h3>
-          <h3 className="m-2"> dog_size - {dogData?.dog_size} </h3>
-          <h3 className="m-2"> status - {dogData?.status} </h3>
+          <h3 className="m-2"> dog_gender - {getDogGender(dog_gender)} </h3>
+          <h3 className="m-2"> dog_size - {getDogSize(dog_size)} </h3>
+          <h3 className="m-2"> status - {getDogStatus(status)} </h3>
           <h3 className="m-2"> general - {general} </h3>
-          <h3 className="m-2"> home_cats - {dogData?.home_cats} </h3>
+          <h3 className="m-2"> at_rescue - {at_rescue} </h3>
+          <h3 className="m-2"> home_cats - {home_cats} </h3>
           <h3 className="m-2"> home_dogs - {dogData?.home_dogs} </h3>
           <h3 className="m-2"> home_animals - {dogData?.home_animals} </h3>
           <h3 className="m-2"> home_children - {dogData?.home_children} </h3>
